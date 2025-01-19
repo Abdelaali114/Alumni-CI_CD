30a627b194fd5aea3496a84a4dd24aa5b7da6ece
Abdelaali114 <abdellaali2550@gmail.com>
2025-01-19 14:42:43 +0100

    jadid

---
+++ ops/tests/integration.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# curl http://localhost:80/welcome
+# - should output -
+# Hello Client! There is one record in the database for Pedro Tavares
+
+#export STR=$(curl http://client:8081/welcome)
+export STR=$(curl http://client-1:80/welcome)
+export SUB='Hello Client! There is one record in the database for Pedro Tavares'
+if [[ "$STR" != *"$SUB"* ]]; then
+  echo 'Integration test failed!'
+  echo 'App output = ' $STR
+  exit 1 # integration test failed
+fi
+
+echo 'Integration test passed. The app returned: ' $STR
